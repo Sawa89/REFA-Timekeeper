59fc4bc81c83ce8fc5cda766d410545322083b70
Philipp Knippschild <phikni@outlook.de>
2018-11-11 23:10:22 +0100

    small changes

--- REFA_GUI.py
+++ REFA_GUI.py
@@ -1,12 +1,19 @@
-##################################################################################################################################
-###                                                                                                                             ##
-#                                                                                                                               ##
-# Grafische Benutzeroberfläche für die REFA - Zeitaufnahme, Copyright 2018 by Philipp Knippschild                               ##
-# Entwickelt mit Python und Qt (PyQt)                                                                                           ##   
-###                                                                                                                             ##
-##################################################################################################################################
-#pylint:disable=E1101
-#pylint:disable=E0611
+# Copyright 2017 Philipp Knippschild 
+#
+# This file is part of REFA-Timerkeeper.
+#
+# REFA-Timekeeper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# REFA-Timekeeper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QRunnable, QThreadPool, QThread, QCoreApplication
